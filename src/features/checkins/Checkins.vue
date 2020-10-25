30db9e2e3d4f8b71e1a01b8926343614407db1f5
Matthew Evans <matthew@evans99.co.uk>
2020-10-25 15:40:53 +0000

    feat(checkins table): filter ui

--- src/features/checkins/Checkins.vue
+++ src/features/checkins/Checkins.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="checkins-container">
+    <div class="filter border border-bottom-0">
+      <div class="row">
+        <div class="col">
+          <label for="venueSelect">Venue</label>
+          <select id="venueSelect" v-model="filterVenue" class="form-select">
+            <option v-for="venue in venues" v-bind:key="venue.id" :value="venue.id">{{ venue.name }}</option>
+          </select>
+        </div>
+        <div class="col">
+          <label for="userSelect">User</label>
+          <select id="userSelect" v-model="filterUser" class="form-select">
+            <option v-for="user in users" v-bind:key="user.id" :value="user.id">{{ user.name }}</option>
+          </select>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <button class="btn btn-outline-secondary" @click="resetFilter">Reset filters</button>
+        </div>
+      </div>
+    </div>
     <CheckinsTable :checkins="checkins"></CheckinsTable>
   </div>
 </template>
@@ -7,6 +28,7 @@
 <script>
 import {Selectors} from "@/shared/services/Store";
 import CheckinsTable from "@/features/checkins/component/CheckinsTable";
+import {identity, pickBy} from "lodash";
 
 export default {
   name: "Checkins",
@@ -16,6 +38,36 @@ export default {
   setup() {
     return {
       checkins: Selectors.nestedCheckins,
+      venues: Selectors.venues,
+      users: Selectors.users,
+    }
+  },
+  data() {
+    return {
+      filterVenue: undefined,
+      filterUser: undefined,
+    }
+  },
+  mounted() {
+    this.filterVenue = this.$route.query['venue'];
+    this.filterUser = this.$route.query['user'];
+  },
+  watch: {
+    filterVenue(venue) {
+      this.updateQueryParams({venue});
+    },
+    filterUser(user) {
+      this.updateQueryParams({user});
+    }
+  },
+  methods: {
+    updateQueryParams(data) {
+      const query = pickBy({...this.$route.query, ...data}, identity);
+      this.$router.replace({query});
+    },
+    resetFilter() {
+      this.filterVenue = undefined;
+      this.filterUser = undefined;
     }
   }
 }
@@ -25,5 +77,17 @@ export default {
 .checkins-container {
   padding: 2rem;
   width: 100%;
+}
+
+.filter {
+  padding: 1rem;
+}
+
+.filter .row {
+  margin-bottom: 1rem;
+}
+
+.filter .row:last-child {
+  margin-bottom: 0;
 }
 </style>
